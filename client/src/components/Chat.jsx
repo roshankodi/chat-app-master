8be431b7e665c58b991f19f2d0ac6d7d722b9ca0
roshankodi <roshankodi019@gmail.com>
2026-05-14 22:53:09 +0530

    Connected frontend to production backend

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://synctalk-backend-w7lj.onrender.com");
 
 function Chat() {
   const [messages, setMessages] = useState([]);
